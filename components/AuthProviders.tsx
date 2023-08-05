e3a653427521b6f1a05e6dcab97a015771ecc0b8
smige1 <egimsikana@gmail.com>
2023-08-05 22:49:56 +0100

    update

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -23,7 +23,7 @@ const AuthProviders = () => {
     useEffect(() => {
         const fetchProviders = async () => {
             const res = await getProviders();
-    
+            console.log(res)
             setProviders(res);
         }
 
